900ee9463c25c301cccaa57f55f00ad05271d4da
QuangNgoVan <quangngovan98@gmail.com>
2022-09-23 21:51:39 +0700

    update code

--- src/components/location/index.js
+++ src/components/location/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import LazyLoad from 'react-lazyload';
 
-import { anHoiLocation } from '../../config';
+import { anHoiLocation, tiecCuoiLocation, thanhHonLocation } from '../../config';
 import Sectiontitle from '../section-title';
 import story1 from '../../images/events/1.jpg';
-// import story2 from '../../images/events/2.jpg';
+import story2 from '../../images/events/2.jpg';
+import story3 from '../../images/events/3.jpg';
 
 import './style.css';
 
@@ -28,8 +29,8 @@ const Location = () => {
               </div>
               <div className="col-lg-7">
                 <div className="ceromony-content">
-                  <h3>Lễ thành hôn</h3>
-                  <span className="time">14 giờ thứ 7, ngày 01 tháng 10 năm 2022</span>
+                  <h3>Lễ ăn hỏi</h3>
+                  <span className="time">8 giờ thứ 6, ngày 30 tháng 09 năm 2022</span>
                   <span className="location">
                     Thôn Mỹ Cầu, Xã Đồng Tân, Huyện Ứng Hoà, TP Hà Nội
                   </span>
@@ -46,35 +47,62 @@ const Location = () => {
             </div>
           </div>
         </div>
-        {/*<div className="Ceremony-wrap">*/}
-        {/*  <div className="row">*/}
-        {/*    <div className="col-lg-7">*/}
-        {/*      <div className="ceromony-content ceromony-content2">*/}
-        {/*        <h3>Tiệc cưới</h3>*/}
-        {/*        <span className="time">11h trưa Chủ nhật, ngày 11 tháng 4 năm 2021</span>*/}
-        {/*        <span className="location">*/}
-        {/*          Cung Eden, trung tâm tiệc cưới Cát Bi Plaza, số 1 Lê Hồng Phong, quận Ngô Quyền,*/}
-        {/*          thành phố Hải Phòng*/}
-        {/*        </span>*/}
-        {/*        <a*/}
-        {/*          className="locationMap"*/}
-        {/*          href={damCuoiLocation}*/}
-        {/*          target="_blank"*/}
-        {/*          rel="noreferrer noopener"*/}
-        {/*        >*/}
-        {/*          Xem địa điểm trên Google Maps*/}
-        {/*        </a>*/}
-        {/*      </div>*/}
-        {/*    </div>*/}
-        {/*    <div className="col-lg-5">*/}
-        {/*      <div className="ceromony-img">*/}
-        {/*        <LazyLoad height="100%">*/}
-        {/*          <img src={story2} alt="Tiệc cưới" />*/}
-        {/*        </LazyLoad>*/}
-        {/*      </div>*/}
-        {/*    </div>*/}
-        {/*  </div>*/}
-        {/*</div>*/}
+        <div className="Ceremony-wrap">
+          <div className="row">
+            <div className="col-lg-7">
+              <div className="ceromony-content ceromony-content2">
+                <h3>Tiệc cưới</h3>
+                <span className="time">9 giờ 30 phút thứ 7, ngày 01 tháng 10 năm 2022</span>
+                <span className="location">Thôn Mỹ Cầu, Xã Đồng Tân, Huyện Ứng Hoà, TP Hà Nội</span>
+                <a
+                  className="locationMap"
+                  href={tiecCuoiLocation}
+                  target="_blank"
+                  rel="noreferrer noopener"
+                >
+                  Xem địa điểm trên Google Maps
+                </a>
+              </div>
+            </div>
+            <div className="col-lg-5">
+              <div className="ceromony-img">
+                <LazyLoad height="100%">
+                  <img src={story2} alt="Tiệc cưới" />
+                </LazyLoad>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="service-area-menu">
+          <div className="Ceremony-wrap">
+            <div className="row">
+              <div className="col-lg-5">
+                <div className="ceromony-img">
+                  <LazyLoad height="100%">
+                    <img src={story3} alt="Lễ vu quy" />
+                  </LazyLoad>
+                </div>
+              </div>
+              <div className="col-lg-7">
+                <div className="ceromony-content">
+                  <h3>Lễ thành hôn</h3>
+                  <span className="time">14 giờ thứ 7, ngày 01 tháng 10 năm 2022</span>
+                  <span className="location">
+                    Thôn An Cư, Xã Trầm Lộng, Huyện Ứng Hoà, TP Hà Nội
+                  </span>
+                  <a
+                    className="locationMap"
+                    href={thanhHonLocation}
+                    target="_blank"
+                    rel="noreferrer noopener"
+                  >
+                    Xem địa điểm trên Google Maps
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,11 @@
 export const anHoiLocation = 'https://goo.gl/maps/jh6bwfMSdMN2B7T69';
 
-export const damCuoiLocation = 'https://goo.gl/maps/jh6bwfMSdMN2B7T69';
+export const tiecCuoiLocation = 'https://goo.gl/maps/jh6bwfMSdMN2B7T69';
+
+export const thanhHonLocation = 'https://goo.gl/maps/ukWfeHJNqCuzH6db8';
 
 export const facebookCoDau = 'https://www.facebook.com/thao.xiteen.39';
 
-export const facebookChuRe = 'https://www.facebook.com/profile.php';
+export const facebookChuRe = 'https://www.facebook.com/tuans.pham.12';
 
 export const rsvpFormUrl = '';
